f952bc538bd4d18739fcc0f9ff3a479d0d25cde9
WanNing <khongwn06@gmail.com>
2026-03-07 22:26:11 +0800

    add PPS_asg.cpp

---
+++ PPS_asg.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string s;
+
+	cout<<"Hello Assignment!"<<endl;
+
+	cout<<"Enter 'start' : ";
+	cin>>s;
+
+	if(s == "start"){
+		cout<<"Monthly Car Parking Pass Management System"<<endl;
+	}
+	else{
+		cout<<"Please enter 'start' to continue."<<endl;
+	}
+	
+	return 0;
+}
