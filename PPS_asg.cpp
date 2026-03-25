c6790a3258c56189857e1376976d12eed7da8828
WNing <khongwn06@gmail.com>
2026-03-25 17:26:05 +0800

    Refactor home and admin page options display

--- PPS_asg.cpp
+++ PPS_asg.cpp
@@ -2,6 +2,8 @@
 #include <unistd.h> //use sleep() for loading screen
 #include <cstdlib>
 #include <limits> //limited output after enter Alphabet
+#include <fstream>
+#include <sstream>
 
 using namespace std;
 
@@ -13,7 +15,9 @@ void newReg_page();
 void stud_page();
 
 //Admin Part
+bool admin_login();
 bool admin_page();
+
 void app_approval();
 void analytic();
 void report();
@@ -60,24 +64,24 @@ bool home_page()
 {
 	int homeChoice;
 	
+	cout<<"UTAR Monthly Car Parking Pass System"<<endl<<endl;
+	cout<<"----------Home Page----------"<<endl;
 	do{
-	cout<<"\nUTAR Monthly Car Parking Pass System"<<endl<<endl;
-	cout<<"--------Home Page--------"<<endl<<endl;
-	cout<<"Select an option : "<<endl;
-	cout<<"1. New Registration Page"<<endl;
-	cout<<"2. Student Page"<<endl;
-	cout<<"3. Admin Page"<<endl;
-	cout<<"4. Exit System"<<endl<<endl;
-	cout<<"Enter your option : ";
-	cin>>homeChoice;
-	
-	switch(homeChoice){
-		case 1: newReg_page(); break;
-		case 2: stud_page(); break;
-		case 3: admin_page(); break;
-		case 4: exit_system(); break;
-		default:limit_input(); cout<<"Invalid Option! Please Enter the Number 1-4."<<endl;
-	}
+		cout<<"\nSelect an option : "<<endl;
+		cout<<"1. New Registration Page"<<endl;
+		cout<<"2. Student Page"<<endl;
+		cout<<"3. Admin Page"<<endl;
+		cout<<"4. Exit System"<<endl<<endl;
+		cout<<"Enter your option : ";
+		cin>>homeChoice;
+		
+		switch(homeChoice){
+			case 1: newReg_page(); break;
+			case 2: stud_page(); break;
+			case 3: admin_page(); break;
+			case 4: exit_system(); break;
+			default:limit_input(); cout<<"Invalid Option! Please Enter the Number 1-4."<<endl;
+		}
 	}while (homeChoice != 4);
 }
 
@@ -90,35 +94,73 @@ void stud_page(){
 }
 
 //Admin Part
+bool admin_login()
+{
+	string id, username, password;
+	string adminUsername, adminPassword;
+
+	cout<<"\n---------Admin Login---------"<<endl<<endl;
+	do{
+		cout<<"Username: ";
+		cin>>adminUsername;
+		cout<<"Password: ";
+		cin>>adminPassword;
+		
+		ifstream file("admin.txt");
+		
+		//If file doesn't exist, create default admin
+	    if(!file){
+	        ofstream newFile("admin.txt");
+	        newFile<<"A001|admin|1234\n";
+	        newFile.close();
+	        file.open("admin.txt"); // reopen for reading
+	    }
+		string line;
+	    while(getline(file, line)){
+	        stringstream ss(line);
+	        getline(ss, id, '|');
+	        getline(ss, username, '|');
+	        getline(ss, password, '|');
+	
+	        if(adminUsername != username || adminPassword != password){
+	            cout<<"Login Failed! Please enter again: "<<endl<<endl;
+	        }
+	    }
+	}while(adminUsername != username || adminPassword != password);
+	cout<<"Login Successful!"<<endl;
+}
+
 bool admin_page()
 {
+	admin_login();
 	loading_screen();
 	
 	int adminChoice;
 	clear_screen();
 	
+	cout<<"----------Admin Page----------"<<endl;
 	do{
-	cout<<"\n--------Admin Page--------"<<endl<<endl;
-	cout<<"Select an option : "<<endl;
-	cout<<"1. Application Approval"<<endl;
-	cout<<"2. Analytics"<<endl;
-	cout<<"3. Reports"<<endl;
-	cout<<"4. Back to Home Page"<<endl;
-	cout<<"5. Exit System"<<endl<<endl;
-	
-	cout<<"Enter your option : ";
-	cin>>adminChoice;
-	
-	switch(adminChoice){
-		case 1: app_approval(); break;
-		case 2: analytic(); break;
-		case 3: report(); break;
-		case 4:	loading_screen(); clear_screen(); home_page(); break;
-		case 5: exit_system(); break;
-		default:limit_input(); cout<<"Invalid Option! Please Enter the Number 1-5."<<endl;
-	}
+		cout<<"\nSelect an option : "<<endl;
+		cout<<"1. Application Approval"<<endl;
+		cout<<"2. Analytics"<<endl;
+		cout<<"3. Reports"<<endl;
+		cout<<"4. Back to Home Page"<<endl;
+		cout<<"5. Exit System"<<endl<<endl;
+		
+		cout<<"Enter your option : ";
+		cin>>adminChoice;
+		
+		switch(adminChoice){
+			case 1: app_approval(); break;
+			case 2: analytic(); break;
+			case 3: report(); break;
+			case 4:	loading_screen(); clear_screen(); home_page(); break;
+			case 5: exit_system(); break;
+			default:limit_input(); cout<<"Invalid Option! Please Enter the Number 1-5."<<endl;
+		}
 	}while (adminChoice != 5);
 }
+
 void app_approval(){
 	cout<<"Application Approval"<<endl;
 }
